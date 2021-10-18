aedee7695ca29fbccb7bcefa670f2512ccc49310
Chen Yifan <chen.yiafn@gmail.com>
2021-10-18 18:29:01 +0800

    Added main orchestrating function + DB helper functions

--- src/main/java/RRPSS.java
+++ src/main/java/RRPSS.java
@@ -1,5 +1,76 @@
+/**
+ Main orchestrating function. THIS IS NOT A GOD CLASS.
+ @authors Gotwin, Ryan, Yifan, Daniel, Christopher
+ @version 1.0
+ @since 2021-10-18
+*/
+
+import java.util.Scanner;
+
+/**
+ * Main Class
+ */
 public class RRPSS {
+    /**
+     * Main ochestrating class.
+     * @param args default
+     */
     public static void main(String[] args){
+        //init your managers + other inits here
+        Scanner sc = new Scanner(System.in);
+
+        //main orchestrating loop
+        int opt;
+        do {
+            printMenu();
+            //handle unsafe inputs
+            String unsafeInput = sc.next();
+            if (unsafeInput.matches("\\d+")){
+                opt = Integer.parseInt(unsafeInput);
+            }
+            else{
+                opt = -1;
+            }
+            //Selection of option
+            switch(opt) {
+                case 0:
+                    System.out.println("Goodbye!");
+                    break;
+                case 1:
+                    System.out.println("This is option 1, do something");
+                    break;
+                case 2:
+
+                    break;
+                default:
+                    System.out.println("Please enter an integer from 0 - 16!");
+            }
+        } while(opt != 0);
 
     }
+
+    /**
+     * Printing menu.
+     */
+    public static void printMenu(){
+        System.out.println("1. Create menu item.\n" +
+                "2. Update menu item.\n" +
+                "3. Remove menu item.\n\n" +
+                "4. Create promotion.\n" +
+                "5. Update promotion.\n" +
+                "6. Remove promotion.\n\n" +
+                "7. Create order.\n" +
+                "8. View order.\n" +
+                "9. Add order items to order.\n" +
+                "10. Remove order items from order.\n\n" +
+                "11. Create reservation booking.\n" +
+                "12. Check reservation booking.\n" +
+                "13. Remove reservation booking.\n\n" +
+                "14. Check table availability.\n\n" +
+                "15. Print order invoice.\n" +
+                "16. Print sale revenue report by period.\n\n" +
+                "0. Exit.");
+    }
+
+
 }
--- src/main/java/StaticClasses/Database.java
+++ src/main/java/StaticClasses/Database.java
@@ -1,4 +1,163 @@
+/**
+ Database static object to CRUD CSV
+ @author Chen Yifan
+ @version 1.0
+ @since 2021-10-18
+*/
 package StaticClasses;
 
-public final class Database {
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.*;
+import com.opencsv.*;
+import com.opencsv.exceptions.CsvException;
+import com.opencsv.exceptions.CsvValidationException;
+
+/**
+ * This static class implements helper methods.
+ */
+public final class Database{
+    /**
+     * The dir path of the CSV files.
+     */
+    private static String directory = "csv/";
+
+    /**
+     * WriteLine simply writes a line to the CSV.
+     * It assumes that you know what you are doing and doesn't check for PK violation.
+     *
+     * @param csvName "Reservation.csv" or wtv
+     * @param line line format =  {"2", "2021-10-13","2000","5", "Ryan", "995", "8"}; <- String[] type
+     */
+	public static void writeLine(String csvName, String[] line) throws IOException {
+        try (ICSVWriter writer = new CSVWriterBuilder(
+                new FileWriter(directory + csvName, true))
+                .withQuoteChar(CSVWriter.NO_QUOTE_CHARACTER)
+                .withSeparator(';')
+                .build()) {
+            writer.writeNext(line);
+        }
+	}
+    /**
+     *
+     * @param csvName The name of the CSV.
+     * Pulls all data from a CSV.
+     * @return An arraylist of hashmaps for all of the values in the CSV
+     * @throws CsvException if csv exception
+     * @throws IOException if fileio exception
+     */
+	public static ArrayList<HashMap<String,String>> readAll(String csvName) throws CsvException, IOException {
+        try (CSVReader reader = new CSVReader(new FileReader(directory + csvName))) {
+            //reading everything
+            List<String[]> r = reader.readAll();
+            //return obj
+            ArrayList<HashMap<String,String>> dat = new ArrayList<>();
+            //splitting headers
+
+            String[] headers = r.get(0)[0].split(";");
+            //parsing each line and assigning it to a hashmap
+            for (int i = 1; i < r.size(); i++){
+                HashMap<String, String> tempHM = new HashMap<>();
+                String[] tempAr = r.get(i)[0].split(";");
+                for (int j = 0; j < tempAr.length; j++){
+                    tempHM.put(headers[j], tempAr[j]);
+                }
+                dat.add(tempHM);
+            }
+            return dat;
+        }
+	}
+
+    /**
+     * Assumes you know what you are doing and PKs are unique.
+     * Finds PK, removes it and rewrites the whole CSV. (inefficient i know)
+     * @param csvName name of csv file
+     * @param primaryKey pk
+     * @param line line to write format =  {"2", "2021-10-13","2000","5", "Ryan", "995", "8"}; <- String[] type
+     * @return whether updated or not
+     * @throws IOException File IO Exception
+     * @throws CsvException CSV Exception
+     */
+	public static boolean updateLine(String csvName, String primaryKey, String[] line) throws IOException, CsvException {
+        try (var fr = new FileReader(directory + csvName, StandardCharsets.UTF_8);
+             var reader = new CSVReader(fr)) {
+            //reads everything
+            String[] nextLine;
+            List<String[]> writeDat = new ArrayList<>();
+            nextLine = reader.readNext();
+            //parse header
+            nextLine = nextLine[0].split(";");
+            writeDat.add(nextLine);
+            boolean updated = false;
+            //parse each row, skip adding to write arraylist if pk same, reformat to writable format
+            while ((nextLine = reader.readNext()) != null) {
+                nextLine = nextLine[0].split(";");
+                if (!nextLine[0].equals(primaryKey)){
+                    writeDat.add(nextLine);
+                }
+                else{
+                    updated = true;
+                }
+            }
+            //if skipped, perform update
+            if (updated){
+                try (ICSVWriter writer = new CSVWriterBuilder(
+                    new FileWriter(directory + csvName))
+                    .withQuoteChar(CSVWriter.NO_QUOTE_CHARACTER)
+                    .withSeparator(';')
+                    .build()) {
+                writer.writeAll(writeDat);
+                }
+                return true;
+            }
+            return false;
+
+        }
+
+	}
+
+    /**
+     * Essentially the same code as update, however, it doesn't "modify" anything
+     * @param csvName csvName
+     * @param primaryKey pk
+     * @return whether or not successfully removed
+     * @throws IOException file io exception
+     * @throws CsvValidationException csv exception
+     */
+	public static Boolean removeLine(String csvName, String primaryKey) throws IOException, CsvValidationException {
+		try (var fr = new FileReader(directory + csvName, StandardCharsets.UTF_8);
+             var reader = new CSVReader(fr)) {
+            //reads everything
+            String[] nextLine;
+            List<String[]> writeDat = new ArrayList<>();
+            nextLine = reader.readNext();
+            //parse header
+            nextLine = nextLine[0].split(";");
+            writeDat.add(nextLine);
+            boolean updated = false;
+            //parse each row, skip adding to write arraylist if pk same, reformat to writable format
+            while ((nextLine = reader.readNext()) != null) {
+                nextLine = nextLine[0].split(";");
+                if (!nextLine[0].equals(primaryKey)){
+                    writeDat.add(nextLine);
+                }
+                else{
+                    updated = true;
+                }
+            }
+            //if skipped, perform update
+            if (updated){
+                try (ICSVWriter writer = new CSVWriterBuilder(
+                    new FileWriter(directory + csvName))
+                    .withQuoteChar(CSVWriter.NO_QUOTE_CHARACTER)
+                    .withSeparator(';')
+                    .build()) {
+                writer.writeAll(writeDat);
+                }
+                return true;
+            }
+            return false;
+
+        }
+	}
 }
