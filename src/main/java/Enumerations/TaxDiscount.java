c2681b6b220274e6f8e829aede0a8ec6aa86637a
christopher-oyh <ongyewhan@gmail.com>
2021-10-23 20:36:29 +0800

    Added TaxDiscount Constant Class

---
+++ src/main/java/Enumerations/TaxDiscount.java
@@ -0,0 +1,7 @@
+package Enumerations;
+
+public final class TaxDiscount {
+    static final double GST = 0.07; 
+    static final double SERVICE_CHARGE = 0.1;
+    static final double MEMBER_DISCOUNT = 0.1;
+}
