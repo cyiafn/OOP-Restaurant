410c9355d7aa0260e4612bbd938d4c5cad62d3e0
gotwinn <32995201+gotwinn@users.noreply.github.com>
2021-11-05 15:26:03 +0800

    Update Database.java

--- src/main/java/StaticClasses/Database.java
+++ src/main/java/StaticClasses/Database.java
@@ -6,6 +6,7 @@
  */
 package StaticClasses;
 
+
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
@@ -17,6 +18,19 @@ import com.opencsv.exceptions.CsvValidationException;
 import groovy.json.JsonBuilder;
 import groovy.json.JsonOutput;
 import groovy.json.JsonSlurper;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+import java.util.Scanner;
+import EntityClasses.MenuItem;
+import EntityClasses.Order;
 
 
 /**
@@ -235,13 +249,120 @@ public final class Database{
      * DELETE FILE FUNCTION
      * @param filepath
      */
-    public static void DeleteFile(String filepath)  {
-            File myObj = new File(filepath.trim());
-            if (myObj.delete()) {
-                System.out.println("Deleted the file: " + myObj.getName());
-            } else {
-                System.out.println("Failed to delete the file.");
+    public static void DeleteFile(String filepath) {
+        File myObj = new File(filepath.trim());
+        if (myObj.delete()) {
+            System.out.println("Deleted the file: " + myObj.getName());
+        } else {
+            System.out.println("Failed to delete the file.");
+        }
+    }
+
+    public static final String SEPARATOR = ";";
+    public static void saveOrder(String filename, List orderList) throws IOException {
+        ArrayList<String> ordersw = new ArrayList<String>();
+        for (int i = 0; i < orderList.size(); i++) {
+            Order order = (Order) orderList.get(i);
+            StringBuilder st = new StringBuilder();
+            st.append(order.getOrderID());
+            st.append(SEPARATOR);
+            st.append(order.getReservationID());
+            st.append(SEPARATOR);
+            st.append(order.getDate());
+            st.append(SEPARATOR);
+            st.append(order.getStatus());
+            st.append(SEPARATOR);
+            st.append(order.getStaff());
+            st.append(SEPARATOR);
+
+
+            ArrayList<MenuItem> items = order.getOrderedItems();
+            for(MenuItem item : items) {
+                st.append(item.get_menuItemID());
+                st.append(SEPARATOR);
+                st.append(item.get_name());
+                st.append(SEPARATOR);
+                st.append(item.get_price());
+                st.append(SEPARATOR);
+                st.append(item.get_quantity());
+                st.append(SEPARATOR);
             }
+            ordersw.add(st.toString());
+        }
+
+        Database.writeOrder(filename, ordersw);
+    }
+
+   // @Override
+    public static ArrayList<Order> readOrder(String fileName) throws IOException {
+        ArrayList<String> stringArray = (ArrayList<String>) Database.read(fileName);
+        ArrayList<Order> orderList = new ArrayList<Order>();
+
+
+        for (int i = 0; i < stringArray.size(); i++) {
+            String st = stringArray.get(i);
+            StringTokenizer star = new StringTokenizer(st, SEPARATOR);
+            ArrayList<MenuItem> items = new ArrayList<MenuItem>();
+
+            int orderId = Integer.valueOf(star.nextToken().trim());
+            String reservationID = star.nextToken().trim();
+            String date = star.nextToken().trim();
+            String status = star.nextToken().trim();
+            String staff = star.nextToken().trim();
+
+            while(star.hasMoreTokens()) {
+               // int itemID = Integer.valueOf(star.nextToken().trim());
+                String itemID = star.nextToken().trim();
+                String name = star.nextToken().trim();
+                String desc = "";
+                double price = Double.valueOf(star.nextToken().trim());
+                int type = Integer.valueOf(star.nextToken().trim());
+                MenuItem item = new MenuItem(itemID, name,desc, price, type);
+                //MenuItem item = new MenuItem(itemID, name, price, type);
+                items.add(item);
+            }
+
+            Order order = new Order(orderId,reservationID,date,status,staff,items);
+            orderList.add(order);
+        }
+        System.out.println(stringArray.size() + " Order(s) Loaded.");
+        return orderList;
+    }
+
+    public static void writeOrder(String fileName, List data) throws IOException {
+        PrintWriter out = new PrintWriter(new FileWriter(directory+fileName));
+
+
+        try {
+            for (int i =0; i < data.size() ; i++) {
+                out.println((String)data.get(i));
+            }
+        }
+        finally {
+            out.close();
+        }
+    }
+
+    /**
+     * For DB classes to read data from text file.
+     *
+     * @param fileName
+     *            To determine the file to read from.
+     * @return the data to read from the text file.
+     */
+    public static List read(String fileName) throws IOException {
+        List data = new ArrayList();
+
+        Scanner scanner = new Scanner(new FileInputStream(directory+fileName));
+        try {
+            while (scanner.hasNextLine()){
+                data.add(scanner.nextLine());
+            }
+        }
+        finally{
+            scanner.close();
+        }
+        return data;
     }
 
 }
